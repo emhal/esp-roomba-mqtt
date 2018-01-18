230950e54dde19a0a5a13567f2af74fcb0b90bc9
Frank Lahme <frank_lahme@franklahmesmbp2.speedport.ip>
2018-01-18 01:26:53 +0100

    Changed to ESP-01 board

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
 #include "secrets.h"
 
 #define HOSTNAME "roomba" // e.g. roomba.local
-#define BRC_PIN 14
+#define BRC_PIN 2
 
-#define MQTT_SERVER "10.0.0.2"
-#define MQTT_USER "homeassistant"
+#define MQTT_SERVER "raspberrypi"
+#define MQTT_USER "fhem"
 #define MQTT_COMMAND_TOPIC "vacuum/command"
 #define MQTT_STATE_TOPIC "vacuum/state"
